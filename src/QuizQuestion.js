cfa832f72517ac78a8323b4f95bd77a31a7bec23
Oscar Neira <recioneira912@gmail.com>
2018-07-21 11:27:39 +0300

    module 6 validation

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+  constructor(props){
+    super(props)
+    this.state = {incorrectAnswer: false}
+  }
+
   handleClick(buttonText){
     console.log("buttonText QuizQuestion")
     if(buttonText === this.props.quiz_question.answer){
+      this.setState({incorrectAnswer: false})
       this.props.showNextQuestionHandler()
       console.log("Answer correct")
     }
+    this.setState({incorrectAnswer: true})
     console.log("Answer is not correct")
   }
     render()
@@ -31,6 +38,7 @@ class QuizQuestion extends Component {
                 })}         
               </ul>
             </section>
+            {this.state.incorrectAnswer? <p className='error'> Sorry, that's not right </p>: null}
           </main>
           )
     }
